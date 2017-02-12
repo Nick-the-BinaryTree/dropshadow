6e3331c332a49b420b72af94340d73accd2e1768
Nick-the-BinaryTree <nick11hd@gmail.com>
2017-02-12 17:47:31 -0500

    Working version

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    fields = ['title', 'text']
+    list_display = ('title', 'published_date')
+
+admin.site.register(Post, PostAdmin)
 
